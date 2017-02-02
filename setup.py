cc5e23402693c7a4c8310f7b3c83f843f9ac6920
Dave <dave@d53dev.net>
2017-02-02 22:51:22 +0100

    Added base project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='ringtail-beanpoll',
+      version='0.0.1',
+      description='Ringtail BLE Bean Poller',
+      author='Dave Sere',
+      author_email='dave@d53dev.net',
+      url='https://github.com/d53dave/ringtail-beanpoll',
+      packages=['distutils', 'distutils.command'],
+     )
